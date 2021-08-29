da0fccfdfcb7b77aa7c704e83c1c46355a0538f8
daercimi <daercimi@gmail.com>
2021-08-28 23:47:53 -0500

    questConsult

--- index.js
+++ index.js
@@ -11,18 +11,6 @@ app.get('/', function (req, res) {
 app.post('/webhook', express.json(),function (req, res) {
 
     const agent = new WebhookClient({ request:req, response:res });
-    // console.log(req.body.queryResult.parameters);
-    // console.log('Dialogflow Request headers: ' + JSON.stringify(req.headers));
-    // console.log('Dialogflow Request body: ' + JSON.stringify(req.body));
-   
-    // function welcome(agent) {
-    //   agent.add(`Welcome to my agent!`);
-    // }
-   
-    // function fallback(agent) {
-    //   agent.add(`I didn't understand`);
-    //   agent.add(`I'm sorry, can you try again?`);
-    // }
 
     function meetRespuestas(agent) {
         const data = req.body.queryResult.parameters
@@ -32,10 +20,20 @@ app.post('/webhook', express.json(),function (req, res) {
         agent.add(`Respuesta registrada. \n ¿Algo más en lo que puedo ayudarte?`);
       }
 
+    function consultClient(agent) {
+      const data = req.body.queryResult.parameters
+
+      console.log("datos: ", data);
+
+      agent.add(`Respuesta registrada. \n ¿Algo más en lo que puedo ayudarte?`);
+    }
+
+
+
+    
     let intentMap = new Map();
-    // intentMap.set('Default Welcome Intent', welcome);
-    // intentMap.set('Default Fallback Intent', fallback);
     intentMap.set('meetRespuestas', meetRespuestas);
+    intentMap.set('consultClient', consultClient);
 
     agent.handleRequest(intentMap);
   })
